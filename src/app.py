a91842bee3bd4f83158479b43d13a61f691c8763
Javier Gracia <107503117+Javiergracia47@users.noreply.github.com>
2024-12-09 01:01:53 +0000

    proyecto finalizado

--- src/app.py
+++ src/app.py
@@ -1,42 +1,118 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+# IMPORTACIONES 
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
+# desde el archivo datastructures trae la info
 from datastructures import FamilyStructure
 #from models import Person
 
+#config de la aplicacion
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+# crear una instancia de la clase familyStructures !!!!!!!!!
+# // en una variable creamos un objeto y tiene la estructura de familystructures
+# jackson es el parametro de last_name de nuestro data.structures, este obj tiene disponible agregar, eliminar, objener y listar miembros
+# # ademas tiene la propiedad last name y members, ejm PRINT (JACKSON_FAMILY.LAST_NAME) (ACCEDO AL APELLIDO) 
 
+jackson_family = FamilyStructure("Jackson") #  es un objeto creado a partir de la clase FamilyStructure. Este objeto representa la familia "Jackson".
+#PODEMOS AGREGAR UNA NUEVA FAMILIA
+# perez_family =  FamilyStructure("perez")
+# Agregamos miembros a la familia Jackson
+jackson_family._members.append(
+    {
+        "id": jackson_family._generateId(),
+        "first_name": "John Jackson",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+    }
+)
+
+jackson_family._members.append(
+    {
+        "id": jackson_family._generateId(),
+        "first_name": "Jane Jackson",
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+    }
+)
+
+jackson_family._members.append(
+    {
+        "id": jackson_family._generateId(),
+        "first_name": "Jimmy Jackson",
+        "age": 5,
+        "lucky_numbers": [1]
+    }
+)
+
+
+ #GESTIONA ERRORES DE LA WEB 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+# // CREA LA PAGINA QUE VEMOS EN LA WEB 
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+#este get sirve para obtener todos los miembros
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def all_members():
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+# entrega un listado de los miembros y los almacena en members
+    members = jackson_family.get_all_members() # seria acceder a los objetos de datastructures con jackson y con get_all_members, accedemos a sus miembros
+    return jsonify(members), 200
+
+
+# este get sirve para obtener un solo miembro 
+@app.route('/member/<int:id>', methods=['GET'])
+def one_member(id):
+    member = jackson_family.get_member(id)
+    if member == None: 
+        return "Usuario no encontrado", 400
+    member_dto = {
+        "name" : member["first_name"],
+        "id" : member["id"],
+        "age" : member["age"],
+        "lucky_numbers" : member["lucky_numbers"]
     }
+    return jsonify(member_dto), 200
 
 
-    return jsonify(response_body), 200
+#ELIMINAR MIEMBRO
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_one_member(id):
+    member = jackson_family.delete_member(id)
+    if member == None: 
+        return "User not exist", 400
+    body = {
+       "done": True
+    }
+    return jsonify(body), 200
+
+
+
+# probamos enviando infor con postman 
+# En este codigo recibimos el new member por la api y al objeto jackson family le agregamos el data_new_member que contiene el nuevo miembro (funcion independiente que contiene el new member) 
+# jackson_family.add_member = accedemos al metodo de data structures llamado add_member que se encarga de agregar a la lista e nuevo miembro.
+@app.route('/member', methods=['POST'])
+def add_member():
+   data_new_member = request.json() #recibimos la info del nuevo miembro  en formato json o formate diccionario
+   if not data_new_member:
+       return "Faltan datos en tu solicitud", 400 # solucionar (mostrar error)
+   member_update =  jackson_family.add_member(data_new_member) 
+   return jsonify(member_update), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,59 @@
 
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
+# """
+# update this file to implement the following already declared methods:
+# - add_member: Should add a member to the self._members list
+# - delete_member: Should delete a member from the self._members list
+# - update_member: Should update a member from the self._members list
+# - get_member: Should return a member from the self._members list
+# """
 from random import randint
-
+# los metodos son funcioneso acciones  que dependen de la clase
+# CLASE QUE CONSTRUYE OBJETOS O MAQUETAS
 class FamilyStructure:
-    def __init__(self, last_name):
+    # ESTE SELF (PROPIO) APUNTA A ESTA SOLA CLASE.
+    def __init__(self, last_name): # le paso el apellido por parametro 
         self.last_name = last_name
-
-        # example list of members
+ 
+       #Almaceno a los miebros de mi familia 
         self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    #  genero un id aleatoio para cada mimebro de la familia 
     def _generateId(self):
         return randint(0, 99999999)
 
+
+    # funcion que agrega el miembro a la familia, agregamos el elemnto que recibimos por el parametro members
+    # EL SELF SIRVE PARA APUNTAR O DISPONIBILIZAR A JACKSON_FAMILY TODOS LOS METODOS. CADA NUEVO OBJ QUE PARTE DE LA CLASE TIENE SU PROPIO SELF APELLIDO O NOMBRE.
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        new_member = {
+          "id": self._generateId(),
+           "first_name" : member["first_name"],
+           "age" : member["age"],
+           "lucky_numbers" : member["lucky_numbers"]
+       }
+        self._members.append (new_member)
+        return self._members
+    
+
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+          for member in self._members:
+            if member["id"] == int(id):
+                self._members.remove(member)
+                return True 
+          return None                                            # return # esta fuera del if y fuera del for 
 
+
+
+# MOSTRAR UN SOLO MIEMBRO   
     def get_member(self, id):
         # fill this method and update the return
-        pass
-
-    # this method is done, it returns a list with all the family members
+        for member in self._members:
+            if member["id"] == int(id):
+                return member  
+       # return # esta fuera del if y fuera del for 
+        return None 
+  # MEOTODO PARA MOSTRAR TODOS LOS MIEMBROS
     def get_all_members(self):
         return self._members
